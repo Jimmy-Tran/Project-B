d6be587f41f23eccea2936c60b0587238581a539
Henno0701 <1048091@hr.nl>
2023-03-30 16:41:22 +0200

    Userlogin change

--- Presentation/UserLogin.cs
+++ Presentation/UserLogin.cs
@@ -5,7 +5,7 @@ static class UserLogin
 
     public static void Start()
     {
-        Console.WriteLine("Welcome to the login page");
+        Console.WriteLine("Welkom op het inlog scherm");
         Console.WriteLine("Please enter your email address");
         string email = Console.ReadLine();
         Console.WriteLine("Please enter your password");
