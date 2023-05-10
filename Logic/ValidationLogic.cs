8df79f936a0d3b998e3f52f8cb5d831a795bbf98
joelhogeschool <124694056+joelhogeschool@users.noreply.github.com>
2023-05-10 16:36:55 +0200

    update registration
    better syntax errors while entering password

--- Logic/ValidationLogic.cs
+++ Logic/ValidationLogic.cs
@@ -11,7 +11,7 @@ namespace Project_B.Logic
     public static class ValidationLogic
     {
         public static bool IsValidEmail(string email) {
-            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.(com|net|org|gov|nl)$")) {
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+.[^@\s]+$")) {
                 Console.WriteLine("De email heeft niet de juiste syntax, probeer het opnieuw");
                 return false;
 
@@ -31,6 +31,26 @@ namespace Project_B.Logic
         public static bool IsNumeric(string number) {
             int tempNumber;
             return int.TryParse(number, out tempNumber);
+        }
+
+        public static bool IsValidPassword(string pass) {
+            if (!Regex.IsMatch(pass, "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$"))
+            {
+                if (!Regex.IsMatch(pass, "[0-9]"))
+                {
+                    Console.WriteLine("Je bent een nummer vergeten in je wachtwoord.");
+                }
+                if (!Regex.IsMatch(pass, "[A-Z]"))
+                {
+                    Console.WriteLine("Je bent een hoofdletter vergeten in je wachtwoord.");
+                }
+                if (pass.Length < 6)
+                {
+                    Console.WriteLine("Het wachtwoord moet minimaal 6 tekens lang zijn");
+                }
+                return false;
+            }
+            return true;
         }
     }    
 }
