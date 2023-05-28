fe1d8977a746e72b6ede585f8d1785fca5de195c
maruf128 <maruf@live.nl>
2023-05-28 14:16:08 +0200

    ervoor gezorgd dat food ook datamodels heeft

--- Logic/FoodMenu.cs
+++ Logic/FoodMenu.cs
@@ -1,30 +1,5 @@
 using System.IO;
 using Newtonsoft.Json;
-public class MenuItem // losse item class
-{
-    public int ID { get; set; }
-    public string Category { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-}
-public class Foodmenu
-{
-    // maak nu een lijst voor iedere gang en drinken
-    public List<MenuItem> Starters { get; set; }
-    public List<MenuItem> Mains { get; set; }
-    public List<MenuItem> Desserts { get; set; }
-    public List<MenuItem> Drinks { get; set; }
-    public List<MenuItem> Wijn { get; set; }
-    public Foodmenu()
-    {
-        Starters = new List<MenuItem>();
-        Mains = new List<MenuItem>();
-        Desserts = new List<MenuItem>();
-        Drinks = new List<MenuItem>();
-        Wijn = new List<MenuItem>();
-    }
-
-}
 // maak de menu importer
 public class MenuImporter
 {
