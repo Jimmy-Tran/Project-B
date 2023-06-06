2cb393cb807eecae7b8911968f098bbc6783ff2e
maruf128 <maruf@live.nl>
2023-06-06 12:42:25 +0200

    Prijs voor de berekeningen word uit json gepakt

--- Logic/Prijs.cs
+++ Logic/Prijs.cs
@@ -1,16 +1,37 @@
 public class Prijs
 {
+    // get the price's
     // Fields for the gangen menu prices
-    public double gangenmenu_2 = 15.50;
-    public double gangenmenu_3 = 20.50;
-    public double gangenmenu_4 = 25.50;
+    public decimal gangenmenu_2;
+    public decimal gangenmenu_3;
+    public decimal gangenmenu_4;
+
+    public Prijs()
+    {
+        Foodmenu menu = MenuImporter.ImportFromJson(@"DataSources/menu.json");
+        foreach (MenuItem x in menu.Gangen)
+        {
+            if (x.Name == "2 Gangen menu")
+            {
+                gangenmenu_2 = x.Price;
+            }
+            else if (x.Name == "3 Gangen menu")
+            {
+                gangenmenu_3 = x.Price;
+            }
+            else if (x.Name == "4 Gangen menu")
+            {
+                gangenmenu_4 = x.Price;
+            }
+        }
+    }
 
     // Field for the discount percentage
-    public double discount = 0.1;
-    public double wijn_arrangement = 3.50;
+    public decimal discount = 0.1M;
+    public decimal wijn_arrangement = 3.50M;
 
     // Method to calculate the price
-    public double prijs(int aantal)
+    public decimal prijs(int aantal)
     {
         // Prompt for the chosen gangen menu
         Console.WriteLine("Welke gangen menu gaat u nemen?");
@@ -62,7 +83,7 @@ public class Prijs
                 // ...
             }
         }
-        double menuPrijs = 0.0;
+        decimal menuPrijs = 0;
 
         // Calculate the price based on the chosen menu
         switch (menu)
@@ -78,20 +99,20 @@ public class Prijs
                 break;
             default:
                 Console.WriteLine("Ongeldige menukeuze.");
-                return 0.0; // Return 0 if an invalid menu choice is entered
+                return 0.0M; // Return 0 if an invalid menu choice is entered
         }
 
         // Calculate the total price
-        double totPrijs = menuPrijs * aantal;
+        decimal totPrijs = menuPrijs * aantal;
 
         // Apply the discount for people under 12
-        double korting = menuPrijs * onder_12 * discount;
+        decimal korting = menuPrijs * onder_12 * discount;
         totPrijs -= korting;
         // zorg nu dat je de wijn erbij doet
         if (wineCount > 0)
         {
             // indien er dus wel mensen zijn met wijn
-            double wijnprijs = wijn_arrangement * wineCount;
+            decimal wijnprijs = wijn_arrangement * wineCount;
             totPrijs += wijnprijs;
         }
         return totPrijs;
