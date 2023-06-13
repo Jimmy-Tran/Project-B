07b0c3c5572d3bd9d5e9ef859718be55fb588413
Jimmy-Tran <jimmytrxn@protonmail.com>
2023-06-13 13:31:04 +0200

    Fixed reservation view

--- Logic/TableLogic.cs
+++ Logic/TableLogic.cs
@@ -11,23 +11,25 @@ namespace Project_B.Logic
     public static class TableLogic
     {
 
-        public static List<string> AvailableTables = new List<string> { "6A", "6B", "4A", "4B", "4C", "4D", "4E", "1", "2", "3", "4", "5", "6", "7", "8", "A", "B", "C", "D", "E", "F", "G", "H" };
+        public static List<string> AvailableTables = new List<string> { "_6A", "_6B", "_4A", "_4B", "_4C", "_4D", "_4E", "_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "A", "B", "C", "D", "E", "F", "G", "H" };
 
 
         public static List<string> CheckTables(DateTime date, TimeSpan timeslot, int persons)
         {
             List<ReservationModel> res = ReservationLogic.GetReservations();  //Get the reservations in an object and loop throught it
-            if (res != null)
-            {
-                foreach (ReservationModel reservartion in res)
-                {
-                    if (reservartion.Date == date && reservartion.TimeSlot == timeslot)
-                    { //Only select the reservation that are given by Date/Time
-                        if (reservartion.Tables != null)
-                        { //Check if the reservation has tablenumbers in it
+            if (res != null) {
+                
+                foreach (ReservationModel reservartion in res) {
+                        Console.WriteLine($"{reservartion.Date} {date} {reservartion.TimeSlot} {timeslot}");
+                    
+                    if (reservartion.Date == date && reservartion.TimeSlot == timeslot) {
+                        Console.WriteLine("Found a match");
+                      //Only select the reservation that are given by Date/Time
+                        if (reservartion.Tables != null) { //Check if the reservation has tablenumbers in it
                             foreach (string i in reservartion.Tables)
                             {
-                                AvailableTables.Remove($"{i}"); //Remove the tablenumber from the available table list
+                                Console.WriteLine(i + "huh");
+                                AvailableTables.Remove($"_{i}"); //Remove the tablenumber from the available table list
                             }
                         }
 
@@ -40,11 +42,11 @@ namespace Project_B.Logic
 
             if (persons == 6 || persons == 5)
             {
-                AvailableTables = AvailableTables.Where(table => table.StartsWith("6") && table.Length == 3).ToList();
+                AvailableTables = AvailableTables.Where(table => table.StartsWith("_6") && table.Length == 3).ToList();
             }
             else if (persons == 4 || persons == 3)
             {
-                AvailableTables = AvailableTables.Where(table => table.StartsWith("4") && table.Length == 3).ToList();
+                AvailableTables = AvailableTables.Where(table => table.StartsWith("_4") && table.Length == 3).ToList();
             }
             else if (persons == 2)
             {
